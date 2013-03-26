23bbd25ee239c96863bc6ea6fd4ba998f5b028a9
Hsiaoming Yang <lepture@me.com>
2013-03-26 14:17:00 +0800

    fix postinstall. use bin instead of binary. fix ansi color

--- root/scripts/postinstall.js
+++ root/scripts/postinstall.js
@@ -4,13 +4,13 @@ try {
   var spm = require('spm');
   spm.plugin.install({
     name: '{%= short_name %}',
-    binary: '{%= name %}',
+    bin: '{%= name %}',
     description: '{%= description %}'
   });
 } catch(e) {
   console.log('  you need install spm to register the program');
   console.log();
-  console.log('    \x1b[31m$ npm install spm@~2.0.0 -g\x1b[39m');
+  console.log('    \x1b[31m$ npm install spm@~2.0.0 -g\x1b[0m');
   console.log();
   console.log("  if you have installed spm, it maybe you haven't set a NODE_PATH environment variable");
   console.log();
